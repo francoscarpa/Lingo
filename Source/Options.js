509a38a0530d13665f6a9c1bbdc654175c0fb53f
Franco Scarpa <scarpafranco92@hotmail.it>
2022-08-07 10:40:42 +0200

    Development.

---
+++ Source/Options.js
@@ -0,0 +1,24 @@
+function ResetOptions() {
+  document.querySelector("#SingleWordInput").value = "https://www.wordreference.com/enit/###";
+  document.querySelector("#MultiWordInput").value = "https://translate.google.it/?hl=it&sl=en&tl=it&text=###&op=translate";
+}
+
+async function RestoreOptions() {
+  const singleWordSetting = await browser.storage.sync.get("singleWord");
+  document.querySelector("#SingleWordInput").value = singleWordSetting.singleWord;
+
+  const multiWordSetting = await browser.storage.sync.get("multiWord");
+  document.querySelector("#MultiWordInput").value = multiWordSetting.multiWord;
+}
+
+document.addEventListener("DOMContentLoaded", RestoreOptions);
+document.querySelector("#SaveButton").addEventListener("click", SaveOptions);
+document.querySelector("#ResetButton").addEventListener("click", ResetOptions);
+
+function SaveOptions(e) {
+  browser.storage.sync.set({
+    singleWord: document.querySelector("#SingleWordInput").value,
+    multiWord: document.querySelector("#MultiWordInput").value,
+  });
+  e.preventDefault();
+}
--- Source/Popup.js
+++ Source/Popup.js
@@ -5,7 +5,7 @@ async function Navigate() {
     if (selectedText != undefined) {
       var selectedTextEscaped = encodeURIComponent(selectedText);
       if (selectedText.split(" ").length === 1) {
-        const setting = await browser.storage.sync.get("singleWord");
+        const singleWordSetting = await browser.storage.sync.get("singleWord");
         window.location = singleWordSetting.singleWord.replace("###", selectedTextEscaped);
       } else {
         const multiWordSetting = await browser.storage.sync.get("multiWord");
@@ -18,28 +18,3 @@ async function Navigate() {
 }
 
 Navigate();
-
-document.addEventListener("DOMContentLoaded", RestoreOptions);
-document.querySelector("#SaveButton").addEventListener("click", SaveOptions);
-document.querySelector("#ResetButton").addEventListener("click", ResetOptions);
-
-function SaveOptions(e) {
-  browser.storage.sync.set({
-    singleWord: document.querySelector("#SingleWordInput").value,
-    multiWord: document.querySelector("#MultiWordInput").value,
-  });
-  e.preventDefault();
-}
-
-async function RestoreOptions() {
-  const singleWordSetting = await browser.storage.sync.get("singleWord");
-  document.querySelector("#SingleWordInput").value = singleWordSetting.singleWord;
-
-  const multiWordSetting = await browser.storage.sync.get("multiWord");
-  document.querySelector("#MultiWordInput").value = multiWordSetting.multiWord;
-}
-
-function ResetOptions() {
-  document.querySelector("#SingleWordInput").value = "https://www.wordreference.com/enit/###";
-  document.querySelector("#MultiWordInput").value = "https://translate.google.it/?hl=it&sl=en&tl=it&text=###&op=translate";
-}
